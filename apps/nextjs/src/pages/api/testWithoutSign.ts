646007c0e1977e6c941df8b92cdf9b9253e91e43
Subodh <subodhmaharjan33@gmail.com>
2023-09-25 08:32:15 +1000

    checking the old way without sign too

---
+++ apps/nextjs/src/pages/api/testWithoutSign.ts
@@ -0,0 +1,22 @@
+ 
+import type { NextApiRequest, NextApiResponse } from "next";
+ 
+import { verifySignature } from "@upstash/qstash/dist/nextjs";
+import { trpc } from "../../utils/trpc";
+ 
+
+const sendNotifications =  async(req:NextApiRequest,res:NextApiResponse)=>
+{
+    const sendNotice =(trpc.post as any).sendNotice.useQuery({token:"dd"});
+     console.log("Lets see no its not my sonthe problem")
+     
+    return res.status(200).json({ message: 'Notifications sent!' });
+};
+
+export default sendNotifications;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
