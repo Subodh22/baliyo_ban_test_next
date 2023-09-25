dc282cbca634824ccd6a045f463033768bde9616
Subodh <subodhmaharjan33@gmail.com>
2023-09-25 17:27:53 +1000

    hungo

--- apps/nextjs/src/pages/api/testWithout.ts
+++ apps/nextjs/src/pages/api/testWithout.ts
@@ -1,15 +1,24 @@
  
 import type { NextApiRequest, NextApiResponse } from "next";
- 
+import {Expo} from 'expo-server-sdk';
 import { verifySignature } from "@upstash/qstash/dist/nextjs";
 import { trpc } from "../../utils/trpc";
  
-
+const expo = new Expo()
 const testWithout =  async(req:NextApiRequest,res:NextApiResponse)=>
 {
     // const sendNotice =(trpc.post as any).sendNotice.useQuery({token:"dd"});
      console.log("Lets see no its not my sonthe problem")
-     
+     const message = {
+      // ExponentPushToken[7-rIfhDlp-HNM5vVK95T6h] -A
+      // ExponentPushToken[LeceGhM18Tt9ilEXjhiA2Y] - i
+      to:"ExponentPushToken[7-rIfhDlp-HNM5vVK95T6h]",
+      title:"Login Reminder",
+      body:"u chubby gr",
+      data:{someData:"u fat fuck"}
+
+    };
+    await expo.sendPushNotificationsAsync([message]);
     return res.status(200).json({ message: 'Notifications sent!' });
 };
 
