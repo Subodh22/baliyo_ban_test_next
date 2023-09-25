da385710b0f4371ee08d4d2103703c87aa76e259
Subodh <subodhmaharjan33@gmail.com>
2023-09-25 18:08:51 +1000

    sucker

--- apps/nextjs/src/pages/api/testWithout.ts
+++ apps/nextjs/src/pages/api/testWithout.ts
@@ -7,17 +7,29 @@ import { trpc } from "../../utils/trpc";
 const expo = new Expo()
 const testWithout =  async(req:NextApiRequest,res:NextApiResponse)=>
 {
-  try {
-    const result =await trpc.post.sendNotice.useQuery({token:"dd"});
-    console.log(result);  // Logs: Notice Sent
-    res.status(200).end();
-  } catch (error) {
+  const { data, error, isLoading } = await trpc.post.sendNotice.useQuery({ token: "dd" });
+
+  if (error) {
     console.error('Error sending notice:', error);
-    res.status(500).end();
+    return res.status(500).end();
   }
-   
-   
+
+  if (isLoading) {
+    // Handle the loading state if necessary. This might not be relevant in an API context.
+    return res.status(202).send("Notification is being processed");
+  }
+
+  if (data) {
+    // Assuming the data contains some relevant information you might want to log or process.
+    console.log(data);
+    return res.status(200).end();
+  }
+
+  // Fallback response in case none of the above conditions are met.
+  return res.status(400).send("Unknown response from trpc");
 };
+   
+ 
 
 export default testWithout;
 
