1b3bc40e8b20008c5b39ef8a30a1ebd216121fd5
Subodh <subodhmaharjan33@gmail.com>
2023-10-05 15:38:23 +1100

    checking with test with cors

--- apps/nextjs/src/pages/api/testWithout.ts
+++ apps/nextjs/src/pages/api/testWithout.ts
@@ -3,8 +3,30 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from 'zod';
 import { verifySignature } from "@upstash/qstash/dist/nextjs";
 import { postRouter } from "@acme/api/src/router/post";
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET', 'POST'],
+  // Add your allowed origins here
+  origin: '*',
+});
+const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: any) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result:any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+};
+
+
+
+
 
 const testWithout = async (req: NextApiRequest, res: NextApiResponse) => {
+  await runMiddleware(req, res, cors);
   try {
     console.log("Received request with body:", req.body); // Log the request body
 
@@ -16,7 +38,7 @@ const testWithout = async (req: NextApiRequest, res: NextApiResponse) => {
     //     path: 'sendNotice',
     //     type: 'query'
     // });
-    const result = await postRouter.alle({
+    const result = await postRouter.sendNotice({
       input: input,
       rawInput:"df",
       ctx: {},
@@ -26,7 +48,7 @@ const testWithout = async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log("Result from postRouter.sendNotice:", result); // Log the result
 
-    res.json({ message: result });
+    res.json({ message: "Result from postRouter.sendNotice:", result});
   } catch (error:any) {
     console.error("Error occurred:", error); // Log the error
     res.status(500).json({ error: error.message });
