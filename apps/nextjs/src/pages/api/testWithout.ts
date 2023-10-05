6d62e031f5a93706c8fb6eab76222aca73784965
Subodh <subodhmaharjan33@gmail.com>
2023-10-05 20:11:55 +1100

    u dummy

--- apps/nextjs/src/pages/api/testWithout.ts
+++ apps/nextjs/src/pages/api/testWithout.ts
@@ -52,7 +52,17 @@ const sendNotice = async () => {
 
 const testWithout = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors);
-   await sendNotice()
+   
+   try {
+    // You can add any logic you want here, if necessary
+
+    // Respond with a simple message
+    await sendNotice()
+    return res.status(200).json({ message: "ok, it's working" });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+
 }
 
 export default testWithout;
